ef68d6db28dd3d80bef0db064d247ba228cdfd35
sami <sami@samy.com>
2024-08-29 20:54:53 -0300

    clase 6

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log(" Holaa!");
+// console.log(" Holaa!");
+
+const h5 = document.querySelector(".card-title"); //.card-title: como en el CSS.
+h5.innerText = "Kiki's Delivery Service";
+
+const p = document.querySelector(".card-text");
+p.innerText = "(1989)";
+
+const img = document.querySelector(".card-img-top");
+img.src = "https://cinemaetceteracom.wordpress.com/wp-content/uploads/2020/04/fsbimnuxj6rrsuf3mn7pfqutws4.jpg?w=1024";
+
+
+// Ponyo (2008), 
+// The Secret World of Arrietty (2010), https://images.amcnetworks.com/ifccenter.com/wp-content/uploads/2019/12/the-secret-world-of-arrietty_592x299-7.jpg
+// My Neighbor Totoro (1988), https://www.nippon.com/en/ncommon/contents/japan-data/2034215/2034215.jpg
